8c404eff1a6291015209622cab07c3dfc9526eda
Abdullah <abdullahlaptop6445@gmail.com>
2025-10-08 14:24:29 +0500

    swaggers added, packages added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config =  new DocumentBuilder().setTitle('AINSS Backend').setDescription('Ainss School management').setVersion('0.1'). build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('ainss-school', app, document); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
